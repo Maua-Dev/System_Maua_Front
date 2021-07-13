7edbb5f0a8c3acea575835928cf6d3971163c12b
Caio Toledo <caio@MBP-de-Caio.bbrouter>
2021-07-13 18:44:28 -0300

    Cores do aplicativo

---
+++ lib/shared/themes/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final darkBlue = Color(0xFF02268F);
+  static final button = Color(0xFF025AC8);
+  static final lightBlue = Color(0xFF0264FF);
+  static final white = Color(0xFFFFFFFF);
+  static final stroke = Color(0xFFE3E3E6);
+  static final shape = Color(0xFFFAFAFC);
+  static final background = Color(0xFFF5F5FA);
+  static final strongLetter = Color(0xFF514766);
+  static final thinLetter = Color(0xFF6E6680);
+  static final red = Color(0xFFFF0000);
+}
