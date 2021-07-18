816e203608ac7ede76e39a930902ccb8e2b59bc3
Fernando <ferd.olvr@gmail.com>
2021-07-18 19:15:17 -0300

    Inicio da arquitetura MVC da splash

---
+++ lib/app/modules/splash/splash_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'splash_controller.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPage createState() => _SplashPage();
+}
+
+class _SplashPage extends ModularState<SplashPage, SplashController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
