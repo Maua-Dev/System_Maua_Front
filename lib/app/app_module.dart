e2b82e6b509dd7a066ada067544d276117dc2a7e
Fernando <ferd.olvr@gmail.com>
2021-07-18 20:34:43 -0300

    Update app_module.dart

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,5 +1,8 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:system_maua_front/app/modules/splash/splash_module.dart';
+import 'package:system_maua_front/app/modules/splash/splash_page.dart';
 
+import 'modules/home/home_module.dart';
 import 'modules/home/home_module.dart';
 
 class AppModule extends Module {
@@ -8,7 +11,7 @@ class AppModule extends Module {
 
   @override
   final List<ModularRoute> routes = [
-    ModuleRoute(Modular.initialRoute, module: HomeModule()),
+    ModuleRoute(Modular.initialRoute, module: SplashModule()),
+    ModuleRoute('/home', module: HomeModule())
   ];
-
 }
