77f18b0f52d4ab983d39c5a03076a886fe6848d2
Fernando <ferd.olvr@gmail.com>
2021-07-18 20:34:31 -0300

    Update splash_page.dart

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:system_maua_front/app/shared/themes/app_images.dart';
+import '../../shared/themes/app_colors.dart';
 import 'splash_controller.dart';
 
-class SplashPage extends StatefulWidget {
-  @override
-  _SplashPage createState() => _SplashPage();
-}
+class SplashPage extends StatelessWidget {
+  SplashPage() {
+    changePage();
+  }
+  void changePage() {
+    Future.delayed(Duration(seconds: 2)).then((value) {
+      Modular.to.pushReplacementNamed('/home');
+    });
+  }
 
-class _SplashPage extends ModularState<SplashPage, SplashController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container(
+      color: AppColors.darkBlue,
+      key: ValueKey('Splash'),
+      child: Center(
+        child: AppImages.logoImage,
+      ),
+    );
   }
 }
