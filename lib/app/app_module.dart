68a53a07d646dea5528ec9059de3e29ae02220af
Fernando <ferd.olvr@gmail.com>
2021-07-19 01:51:23 -0300

    Update app_module.dart

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,8 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:system_maua_front/app/modules/splash/splash_module.dart';
-import 'package:system_maua_front/app/modules/splash/splash_page.dart';
 
-import 'modules/home/home_module.dart';
 import 'modules/home/home_module.dart';
 
 class AppModule extends Module {
