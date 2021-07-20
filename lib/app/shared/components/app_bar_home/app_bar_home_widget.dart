80ef1c5d27a0ba96093d9db68f94cfed9eabb809
GabrielGodoy01 <gabriel.godoybz@hotmail.com>
2021-07-19 23:17:13 -0300

    Update app_bar_home_widget.dart

--- lib/app/shared/components/app_bar_home/app_bar_home_widget.dart
+++ lib/app/shared/components/app_bar_home/app_bar_home_widget.dart
@@ -19,72 +19,72 @@ class AppBarHomeWidget extends StatelessWidget implements PreferredSizeWidget {
       required this.local});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height * 0.24,
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            decoration: BoxDecoration(gradient: AppGradients.blueGradient),
-            child: Padding(
-              padding: EdgeInsets.only(
-                  bottom: MediaQuery.of(context).size.height * 0.03),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text.rich(TextSpan(
-                          text: 'Olá, ',
-                          style: AppTextStyles.appBarHomeBody,
-                          children: [
-                            TextSpan(
-                                text: usuario,
-                                style: AppTextStyles.appBarHomeTitle),
-                          ])),
-                      Text(
-                        ra,
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height * 0.24,
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          decoration: BoxDecoration(gradient: AppGradients.blueGradient),
+          child: Padding(
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).size.height * 0.03),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text.rich(TextSpan(
+                        text: 'Olá, ',
                         style: AppTextStyles.appBarHomeBody,
-                      )
-                    ],
+                        children: [
+                          TextSpan(
+                              text: usuario,
+                              style: AppTextStyles.appBarHomeTitle),
+                        ])),
+                    Text(
+                      ra,
+                      style: AppTextStyles.appBarHomeBody,
+                    )
+                  ],
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.23,
+                  height: MediaQuery.of(context).size.height * 0.13,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: AppColors.white),
+                    borderRadius: BorderRadius.circular(100),
                   ),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.23,
-                    height: MediaQuery.of(context).size.height * 0.13,
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 2, color: AppColors.white),
-                      borderRadius: BorderRadius.circular(100),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(3),
-                      child: Container(
-                          width: MediaQuery.of(context).size.width * 0.20,
-                          height: MediaQuery.of(context).size.height * 0.115,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(100),
-                              image: DecorationImage(
-                                image: NetworkImage(
-                                    'https://avatars.githubusercontent.com/u/24724451?v=4'),
-                              ))),
-                    ),
-                  )
-                ],
-              ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(3),
+                    child: Container(
+                        width: MediaQuery.of(context).size.width * 0.20,
+                        height: MediaQuery.of(context).size.height * 0.115,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  'https://avatars.githubusercontent.com/u/24724451?v=4'),
+                            ))),
+                  ),
+                )
+              ],
             ),
           ),
-          Align(
-            alignment:
-                Alignment(0.0, MediaQuery.of(context).size.height * 0.006),
-            child: AulaCardWidget(
-              materia: materia,
-              duracao: duracao,
-              local: local,
-            ),
-          )
-        ],
-      ),
+        ),
+        Positioned(
+          top: MediaQuery.of(context).size.height * 0.18,
+          right: 0,
+          left: 0,
+          child: AulaCardWidget(
+            materia: materia,
+            duracao: duracao,
+            local: local,
+          ),
+        )
+      ],
     );
   }
 
