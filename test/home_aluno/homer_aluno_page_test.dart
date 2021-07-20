16bb2d459b31d8e7d788334ce061b5b77a067b9f
GabrielGodoy01 <gabriel.godoybz@hotmail.com>
2021-07-19 23:34:18 -0300

    Criação dos teste unitários de widget

---
+++ test/home_aluno/homer_aluno_page_test.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_modular_test/flutter_modular_test.dart';
+import 'package:system_maua_front/app/modules/home-aluno/home_aluno_module.dart';
+
+void main() {
+  initModule(HomeAlunoModule(), initialModule: true);
+}
