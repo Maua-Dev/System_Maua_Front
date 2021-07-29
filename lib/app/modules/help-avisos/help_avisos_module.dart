dfa380d086b959378981de881727b7a1be03dbed
Fernando <ferd.olvr@gmail.com>
2021-07-29 12:11:16 -0300

    Update help_avisos_module.dart

--- lib/app/modules/help-avisos/help_avisos_module.dart
+++ lib/app/modules/help-avisos/help_avisos_module.dart
@@ -11,6 +11,6 @@ class HelpAvisosModule extends Module {
 
   @override
   final List<ModularRoute> routes = [
-    ChildRoute('/', child: (_, args) => HelpAvisosPage()),
+    ChildRoute(Modular.initialRoute, child: (_, args) => HelpAvisosPage()),
   ];
 }
