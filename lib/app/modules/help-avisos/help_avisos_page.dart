0beaf18f003a9f779b4635754c4d67959b91f221
Fernando <ferd.olvr@gmail.com>
2021-07-29 23:16:06 -0300

    Update help_avisos_page.dart

--- lib/app/modules/help-avisos/help_avisos_page.dart
+++ lib/app/modules/help-avisos/help_avisos_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:system_maua_front/app/modules/help-avisos/help_avisos_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:system_maua_front/app/shared/components/app_bar/app_bar_widget.dart';
+import 'package:system_maua_front/app/shared/themes/app_colors.dart';
+import 'package:system_maua_front/app/shared/themes/app_text_styles.dart';
 
 class HelpAvisosPage extends StatefulWidget {
   final String title;
@@ -16,8 +19,9 @@ class HelpAvisosPageState extends State<HelpAvisosPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      appBar: AppBarWidget(
+        title: 'Avisos',
+        icon: Icons.help_outline_outlined,
       ),
       body: Column(
         children: <Widget>[],
