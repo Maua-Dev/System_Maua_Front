05a24bb959f67dba20277262d3c44c73ca3e3197
Fernando <ferd.olvr@gmail.com>
2021-07-29 23:16:03 -0300

    Update helpAvisos_store_test.dart

--- test/app/modules/help-avisos/helpAvisos_store_test.dart
+++ test/app/modules/help-avisos/helpAvisos_store_test.dart
@@ -2,15 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:system_maua_front/app/modules/help-avisos/help_avisos_controller.dart';
 
 void main() {
-  late HelpAvisosStore store;
+  late HelpAvisosController store;
 
   setUpAll(() {
-    store = HelpAvisosStore();
-  });
-
-  test('increment count', () async {
-    expect(store.value, equals(0));
-    store.increment();
-    expect(store.value, equals(1));
+    store = HelpAvisosController();
   });
 }
