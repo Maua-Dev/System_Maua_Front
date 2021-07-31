d755e7a3b6852a891c6d871b62c21fd395008590
Fernando <ferd.olvr@gmail.com>
2021-07-31 02:01:44 -0300

    Create help_aluno_module.dart

---
+++ lib/app/modules/help-aluno/help_aluno_module.dart
@@ -0,0 +1,15 @@
+import 'package:system_maua_front/app/modules/help-aluno/help_aluno_page.dart';
+import 'package:system_maua_front/app/modules/help-aluno/help_aluno_controller.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class HelpAlunoModule extends Module {
+  @override
+  final List<Bind> binds = [
+    Bind.lazySingleton((i) => HelpAlunoController()),
+  ];
+
+  @override
+  final List<ModularRoute> routes = [
+    ChildRoute(Modular.initialRoute, child: (_, args) => HelpAlunoPage())
+  ];
+}
