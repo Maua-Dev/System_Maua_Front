b42a9c696ceeb1f0a91aad777b1b6f1aea90df42
Fernando <ferd.olvr@gmail.com>
2021-07-31 02:02:08 -0300

    Update help_avisos_page.dart

--- lib/app/modules/help-avisos/help_avisos_page.dart
+++ lib/app/modules/help-avisos/help_avisos_page.dart
@@ -2,8 +2,6 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:system_maua_front/app/modules/help-avisos/help_avisos_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:system_maua_front/app/shared/components/app_bar/app_bar_widget.dart';
-import 'package:system_maua_front/app/shared/themes/app_colors.dart';
-import 'package:system_maua_front/app/shared/themes/app_text_styles.dart';
 
 class HelpAvisosPage extends StatefulWidget {
   final String title;
