dae5ed26ab7728701dd6a54854ba3ce634f3b993
Fernando <ferd.olvr@gmail.com>
2021-07-31 02:01:59 -0300

    Update helpAvisos_module_test.dart

--- test/app/modules/help-avisos/helpAvisos_module_test.dart
+++ test/app/modules/help-avisos/helpAvisos_module_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular_test/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:system_maua_front/app/modules/help-avisos/help_avisos_module.dart';
