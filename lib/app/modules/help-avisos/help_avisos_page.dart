7ef52d36a9f82aa3fc4c7ec1798b705480af9cde
Fernando <ferd.olvr@gmail.com>
2021-07-31 02:14:58 -0300

    Update help_avisos_page.dart

--- lib/app/modules/help-avisos/help_avisos_page.dart
+++ lib/app/modules/help-avisos/help_avisos_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:system_maua_front/app/modules/help-avisos/help_avisos_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:system_maua_front/app/shared/components/app_bar/app_bar_widget.dart';
+import 'package:system_maua_front/app/shared/themes/app_colors.dart';
+import 'package:system_maua_front/app/shared/themes/app_text_styles.dart';
 
 class HelpAvisosPage extends StatefulWidget {
   final String title;
@@ -13,7 +15,7 @@ class HelpAvisosPage extends StatefulWidget {
 
 class HelpAvisosPageState extends State<HelpAvisosPage> {
   final HelpAvisosController store = Modular.get();
-
+  bool checkedValue = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +23,71 @@ class HelpAvisosPageState extends State<HelpAvisosPage> {
         title: 'Avisos',
         icon: Icons.help_outline_outlined,
       ),
-      body: Column(
-        children: <Widget>[],
+      body: ListView(
+        children: <Widget>[
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Icon(
+                Icons.warning,
+                size: 150,
+              ),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'Avisos Gerais da reitoria',
+                style: AppTextStyles.bodyBold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.08,
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    Modular.to.navigate('/home');
+                  });
+                },
+                style: ElevatedButton.styleFrom(primary: AppColors.white),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.calendar_today,
+                            color: Colors.black,
+                            size: 20,
+                          ),
+                          Text(
+                            '31/07',
+                            style: TextStyle(color: Colors.black, fontSize: 20),
+                          )
+                        ],
+                      ),
+                    ),
+                    Text(
+                      'Comunicado sobre as provas',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    Checkbox(
+                      value: checkedValue,
+                      onChanged: (checkedValue) =>
+                          setState(() => this.checkedValue = checkedValue!),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
