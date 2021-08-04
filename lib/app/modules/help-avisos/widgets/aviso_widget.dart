6be466907a8cbf4a8624d255f12c8d3f9513f4db
Fernando <ferd.olvr@gmail.com>
2021-08-04 01:16:30 -0300

    Create aviso_widget.dart

---
+++ lib/app/modules/help-avisos/widgets/aviso_widget.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:system_maua_front/app/shared/themes/app_colors.dart';
+import 'package:system_maua_front/app/shared/themes/app_text_styles.dart';
+
+class AvisosCard extends StatefulWidget {
+  final String data;
+  final String titulo;
+  final String descricao;
+
+  const AvisosCard(
+      {Key? key,
+      required this.data,
+      required this.titulo,
+      required this.descricao})
+      : super(key: key);
+
+  @override
+  _AvisosCardState createState() {
+    return _AvisosCardState();
+  }
+}
+
+class _AvisosCardState extends State<AvisosCard> {
+  bool bool_color = false;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.6,
+        child: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              bool_color = !bool_color;
+            });
+          },
+          style: ElevatedButton.styleFrom(primary: Colors.white),
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.08,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.calendar_today,
+                            color: Colors.black,
+                            size: 20,
+                          ),
+                          Text(
+                            widget.data,
+                            style: TextStyle(color: Colors.black, fontSize: 20),
+                          )
+                        ],
+                      ),
+                    ),
+                    Text(
+                      widget.titulo,
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    Icon(
+                      bool_color ? Icons.arrow_drop_up : Icons.arrow_drop_down,
+                      size: 30,
+                      color: Colors.black,
+                    ),
+                  ],
+                ),
+              ),
+              Visibility(
+                visible: bool_color,
+                child: Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Flexible(
+                        child: Text(
+                          widget.descricao,
+                          style: AppTextStyles.lightBody,
+                          textAlign: TextAlign.left,
+                          // overflow: TextOverflow.clip,
+                          // softWrap: true,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+    ;
+  }
+}
