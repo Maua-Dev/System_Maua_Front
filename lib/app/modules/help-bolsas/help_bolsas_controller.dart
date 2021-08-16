7315a0908b6d8d8c97e1ccf7ea51f4ecccf44c07
Fernando <ferd.olvr@gmail.com>
2021-08-16 18:01:12 -0300

    Delete help_bolsas_controller.dart
    Não usado

--- lib/app/modules/help-bolsas/help_bolsas_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:mobx/mobx.dart';
-
-part 'help_bolsas_controller.g.dart';
-
-class BolsasController = BolsasControllerBase with _$BolsasController;
-
-abstract class BolsasControllerBase with Store {}
