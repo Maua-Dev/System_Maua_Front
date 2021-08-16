0aa42ea6c9a97f39088ff7660b2db83bd189fe83
Fernando <ferd.olvr@gmail.com>
2021-08-16 17:59:04 -0300

    Update help_bolsas_page.dart

--- lib/app/modules/help-bolsas/help_bolsas_page.dart
+++ lib/app/modules/help-bolsas/help_bolsas_page.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-import 'package:intl/intl.dart';
-import 'package:system_maua_front/app/modules/help-bolsas/help_bolsas_controller.dart';
 import 'package:system_maua_front/app/shared/components/app_bar/app_bar_widget.dart';
 import 'package:system_maua_front/app/shared/themes/app_text_styles.dart';
 
