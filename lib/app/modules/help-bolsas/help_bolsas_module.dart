afd1eaa8ecf2758aa9516a083a55aaf6ded11414
Fernando <ferd.olvr@gmail.com>
2021-08-16 17:59:01 -0300

    Update help_bolsas_module.dart

--- lib/app/modules/help-bolsas/help_bolsas_module.dart
+++ lib/app/modules/help-bolsas/help_bolsas_module.dart
@@ -1,13 +1,9 @@
 import 'package:flutter_modular/flutter_modular.dart';
-
-import 'help_bolsas_controller.dart';
 import 'help_bolsas_page.dart';
 
 class BolsasModule extends Module {
   @override
-  final List<Bind> binds = [
-    Bind.lazySingleton((i) => BolsasController()),
-  ];
+  final List<Bind> binds = [];
 
   @override
   final List<ModularRoute> routes = [
