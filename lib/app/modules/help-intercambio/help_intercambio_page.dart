333e416644e1cb44f82591ed15ad21bfc3842a05
Fernando <ferd.olvr@gmail.com>
2021-08-19 17:57:07 -0300

    Update help_intercambio_page.dart

---
+++ lib/app/modules/help-intercambio/help_intercambio_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class HelpIntercambioPage extends StatefulWidget {
+  @override
+  _HelpIntercambioPageState createState() => _HelpIntercambioPageState();
+}
+
+class _HelpIntercambioPageState extends State<HelpIntercambioPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
