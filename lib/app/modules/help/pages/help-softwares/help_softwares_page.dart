02e0372e4a5e89f14cd772ff9730ce700e18b766
Fernando <ferd.olvr@gmail.com>
2021-08-21 14:57:23 -0300

    Update help_softwares_page.dart

--- lib/app/modules/help/pages/help-softwares/help_softwares_page.dart
+++ lib/app/modules/help/pages/help-softwares/help_softwares_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:system_maua_front/app/shared/components/app_bar/app_bar_widget.dart';
 
 class HelpSoftwaresPage extends StatefulWidget {
   @override
@@ -8,6 +10,11 @@ class HelpSoftwaresPage extends StatefulWidget {
 class _HelpSoftwaresPageState extends State<HelpSoftwaresPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBarWidget(
+        title: 'Softwares',
+        icon: Icons.computer,
+      ),
+    );
   }
 }
