ddbadc0926a926b4eae7ccddd6018e03c65a30de
Fernando <ferd.olvr@gmail.com>
2021-08-21 14:48:20 -0300

    Create help_softwares_page.dart

---
+++ lib/app/modules/help/pages/help-softwares/help_softwares_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class HelpSoftwaresPage extends StatefulWidget {
+  @override
+  _HelpSoftwaresPageState createState() => _HelpSoftwaresPageState();
+}
+
+class _HelpSoftwaresPageState extends State<HelpSoftwaresPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
