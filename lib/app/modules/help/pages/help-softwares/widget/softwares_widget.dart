9e851921927561330fee8af8f382e263861d0c8d
Fernando <ferd.olvr@gmail.com>
2021-08-24 08:17:43 -0300

    Create softwares_widget.dart

---
+++ lib/app/modules/help/pages/help-softwares/widget/softwares_widget.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:system_maua_front/app/shared/themes/app_text_styles.dart';
+
+class SofwaresCard extends StatefulWidget {
+  final String titulo;
+  final String descricao;
+  final bool isOpen;
+  final Function()? onPressed;
+
+  const SofwaresCard(
+      {Key? key,
+      required this.titulo,
+      required this.descricao,
+      required this.isOpen,
+      this.onPressed})
+      : super(key: key);
+
+  @override
+  _SoftwaresCardState createState() {
+    return _SoftwaresCardState();
+  }
+}
+
+class _SoftwaresCardState extends State<SofwaresCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ExpansionPanelList(
+            animationDuration: Duration(seconds: 1),
+            elevation: 4,
+            expandedHeaderPadding: EdgeInsets.all(8),
+            children: [
+              ExpansionPanel(
+                canTapOnHeader: true,
+                isExpanded: widget.isOpen,
+                headerBuilder: (context, isExpanded) => Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Center(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Expanded(
+                      child: Text(
+                        widget.titulo,
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
+                body: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  child: Text(
+                    widget.descricao,
+                    style: AppTextStyles.lightBody.copyWith(fontSize: 16),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+            ],
+            expansionCallback: (i, isExpanded) {
+              widget.onPressed!();
+            }));
+  }
+}
