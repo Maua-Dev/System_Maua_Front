889fecde79e54786c56beefe2f703b1c4031406f
GabrielGodoy01 <gabriel.godoybz@hotmail.com>
2021-08-25 20:42:46 -0300

    [FM-11] - TESTE

---
+++ lib/app/modules/materias/pages/teste/teste.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Gabriel extends StatelessWidget {
+  const Gabriel({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
