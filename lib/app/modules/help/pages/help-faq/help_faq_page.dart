be7d0d2ea1de977b179df0a7ffe670f11731576a
Fernando <ferd.olvr@gmail.com>
2021-09-01 10:00:29 -0300

    Update help_faq_page.dart

--- lib/app/modules/help/pages/help-faq/help_faq_page.dart
+++ lib/app/modules/help/pages/help-faq/help_faq_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:system_maua_front/app/modules/help/pages/help-faq/widget/faq_widget.dart';
-import 'package:system_maua_front/app/modules/help/pages/help-faq/widget/faq_widget.dart';
 import 'package:system_maua_front/app/shared/components/app_bar/app_bar_widget.dart';
 import 'package:system_maua_front/app/shared/themes/app_text_styles.dart';
 import 'help_faq_controller.dart';
