94ae11d31df296756e06527e386f58bfae0f471e
Fernando <ferd.olvr@gmail.com>
2021-09-01 08:47:49 -0300

    Update help_faq_page.dart

---
+++ lib/app/modules/help/pages/help-faq/help_faq_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-faq/widget/faq_widget.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-faq/widget/faq_widget.dart';
+import 'package:system_maua_front/app/shared/components/app_bar/app_bar_widget.dart';
+import 'package:system_maua_front/app/shared/themes/app_text_styles.dart';
+import 'help_faq_controller.dart';
+
+class HelpFaqPage extends StatefulWidget {
+  @override
+  _HelpFaqPageState createState() => _HelpFaqPageState();
+}
+
+class _HelpFaqPageState extends ModularState<HelpFaqPage, HelpFaqController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarWidget(
+        title: 'FAQ',
+        icon: Icons.help_outline_outlined,
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'Perguntas frequentes',
+                style: AppTextStyles.lightBody,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Expanded(child: Observer(builder: (_) {
+            return ListView.builder(
+              shrinkWrap: true,
+              itemCount: controller.faqAction.length,
+              itemBuilder: (context, index) => FaqCard(
+                titulo: controller.faqAction[index].faq.titulo,
+                descricao: controller.faqAction[index].faq.descricao,
+                isOpen: controller.faqAction[index].isOpen,
+                onPressed: () {
+                  controller.trocaOpen(index);
+                },
+              ),
+            );
+          })),
+          SizedBox(
+            height: 16,
+          )
+        ],
+      ),
+    );
+  }
+}
