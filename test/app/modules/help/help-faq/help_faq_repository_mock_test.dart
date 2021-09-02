54a8400b00de2a892dba9d6b6a17fa4d3289a1bd
Fernando <ferd.olvr@gmail.com>
2021-09-02 10:03:28 -0300

    Update help_faq_repository_mock_test.dart

--- test/app/modules/help/help-faq/help_faq_repository_mock_test.dart
+++ test/app/modules/help/help-faq/help_faq_repository_mock_test.dart
@@ -2,13 +2,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:system_maua_front/app/modules/help/pages/help-faq/repositories/help_faq_repository.dart';
 
 void main() {
-  late HelpFaqRepository helpSoftwaresRepository;
+  late HelpFaqRepository helpFaqRepository;
 
   setUpAll(() {
-    helpSoftwaresRepository = HelpFaqRepository();
+    helpFaqRepository = HelpFaqRepository();
   });
 
   test('[TEST] - getInformacaoFaq', () {
-    expect(helpSoftwaresRepository.listFaq.isNotEmpty, true);
+    expect(helpFaqRepository.listFaq.isNotEmpty, true);
   });
 }
