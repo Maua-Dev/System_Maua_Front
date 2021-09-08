d0e0b6de33435ffc173b78122a597adc6351b224
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2021-09-08 20:04:30 -0300

    [FN-20] - Merge FM-24 motivo: melhor codigo

--- lib/app/modules/mural-divulgacao/pages/mural_vendas_materiais/mural_vendas_materiais_controller.dart
+++ lib/app/modules/mural-divulgacao/pages/mural_vendas_materiais/mural_vendas_materiais_controller.dart
@@ -1,4 +1,7 @@
 import 'package:mobx/mobx.dart';
+import 'package:system_maua_front/app/modules/mural-divulgacao/pages/mural_vendas_materiais/models/vendas_materiais_model.dart';
+import 'package:system_maua_front/app/modules/mural-divulgacao/pages/mural_vendas_materiais/models/vendas_materiais_panel_model.dart';
+import 'package:system_maua_front/app/modules/mural-divulgacao/pages/mural_vendas_materiais/repositories/mural_vendas_materiais_repository_interface.dart';
 
 part 'mural_vendas_materiais_controller.g.dart';
 
@@ -6,4 +9,44 @@ class MuralVendasMateriaisController = _MuralVendasMateriaisControllerBase
     with _$MuralVendasMateriaisController;
 
 abstract class _MuralVendasMateriaisControllerBase with Store {
+  ImuralVendasMateriaisRepository? repository;
+
+  _MuralVendasMateriaisControllerBase(
+    this.repository,
+  );
+
+  @observable
+  List<VendasMateriaisModel> listaVendas = List.empty();
+
+  @observable
+  List<VendasMateriaisPanelModel>listaPanelVendasMateriais = [];
+
+  @action
+  Future<void> getVendas() async {
+    listaVendas = await repository!.getVendas();
+    getListaPanelVendasMateriais();
+  }
+
+  @action
+  void getListaPanelVendasMateriais() {
+    var list = <VendasMateriaisPanelModel>[];
+    for (var i = 0; i < listaVendas.length; i++) {
+      //list.add(VendasMateriaisPanelModel(vendas: listaVendas[i]));
+    }
+    listaPanelVendasMateriais = list;
+  }
+
+  @action
+  void trocaOpen(int index) {
+    var lista = List<VendasMateriaisPanelModel>.from(listaPanelVendasMateriais);
+    lista[index].changeStateIsOpen();
+    listaPanelVendasMateriais = lista;
+  }
+
+
+
+
+
+  
+
 }
--- lib/app/modules/mural-divulgacao/pages/mural_vendas_materiais/mural_vendas_materiais_controller.g.dart
+++ lib/app/modules/mural-divulgacao/pages/mural_vendas_materiais/mural_vendas_materiais_controller.g.dart
@@ -10,10 +10,81 @@ part of 'mural_vendas_materiais_controller.dart';
 
 mixin _$MuralVendasMateriaisController
     on _MuralVendasMateriaisControllerBase, Store {
+  final _$listaVendasAtom =
+      Atom(name: '_MuralVendasMateriaisControllerBase.listaVendas');
+
+  @override
+  List<VendasMateriaisModel> get listaVendas {
+    _$listaVendasAtom.reportRead();
+    return super.listaVendas;
+  }
+
+  @override
+  set listaVendas(List<VendasMateriaisModel> value) {
+    _$listaVendasAtom.reportWrite(value, super.listaVendas, () {
+      super.listaVendas = value;
+    });
+  }
+
+  final _$listaPanelVendasMateriaisAtom = Atom(
+      name: '_MuralVendasMateriaisControllerBase.listaPanelVendasMateriais');
+
+  @override
+  List<dynamic> get listaPanelVendasMateriais {
+    _$listaPanelVendasMateriaisAtom.reportRead();
+    return super.listaPanelVendasMateriais;
+  }
+
+  @override
+  set listaPanelVendasMateriais(List<dynamic> value) {
+    _$listaPanelVendasMateriaisAtom
+        .reportWrite(value, super.listaPanelVendasMateriais, () {
+      super.listaPanelVendasMateriais = value;
+    });
+  }
+
+  final _$getVendasAsyncAction =
+      AsyncAction('_MuralVendasMateriaisControllerBase.getVendas');
+
+  @override
+  Future<void> getVendas() {
+    return _$getVendasAsyncAction.run(() => super.getVendas());
+  }
+
+  final _$_MuralVendasMateriaisControllerBaseActionController =
+      ActionController(name: '_MuralVendasMateriaisControllerBase');
+
+  @override
+  void getListaPanelVendasMateriais() {
+    final _$actionInfo =
+        _$_MuralVendasMateriaisControllerBaseActionController.startAction(
+            name:
+                '_MuralVendasMateriaisControllerBase.getListaPanelVendasMateriais');
+    try {
+      return super.getListaPanelVendasMateriais();
+    } finally {
+      _$_MuralVendasMateriaisControllerBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void trocaOpen(int index) {
+    final _$actionInfo = _$_MuralVendasMateriaisControllerBaseActionController
+        .startAction(name: '_MuralVendasMateriaisControllerBase.trocaOpen');
+    try {
+      return super.trocaOpen(index);
+    } finally {
+      _$_MuralVendasMateriaisControllerBaseActionController
+          .endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
     return '''
-
+listaVendas: ${listaVendas},
+listaPanelVendasMateriais: ${listaPanelVendasMateriais}
     ''';
   }
 }
