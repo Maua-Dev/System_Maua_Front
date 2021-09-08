b05be76cb1e0652b01a6534c64d39ac48f349634
GabrielGodoy01 <gabriel.godoybz@hotmail.com>
2021-09-08 11:47:31 -0300

    [FM-24] - Criação do model de estágios e mock de dados

---
+++ lib/app/modules/mural-divulgacao/pages/mural_estagio/models/estagios_model.dart
@@ -0,0 +1,10 @@
+class EstagiosModel {
+  final String dataEstagio;
+  final String nomeEstagio;
+  final String descricaoEstagio;
+
+  EstagiosModel(
+      {required this.dataEstagio,
+      required this.nomeEstagio,
+      required this.descricaoEstagio});
+}
--- lib/app/modules/mural-divulgacao/pages/mural_estagio/repositories/mural_estagios_repository_interface.dart
+++ lib/app/modules/mural-divulgacao/pages/mural_estagio/repositories/mural_estagios_repository_interface.dart
@@ -1,1 +1,5 @@
-abstract class IMuralEstagiosRepository {}
+import 'package:system_maua_front/app/modules/mural-divulgacao/pages/mural_estagio/models/estagios_model.dart';
+
+abstract class IMuralEstagiosRepository {
+  Future<List<EstagiosModel>> getEstagios();
+}
