10964bbfc8c99e12217a82a31dad1ec5723dd9c7
GabrielGodoy01 <gabriel.godoybz@hotmail.com>
2021-09-14 12:50:14 -0300

    [FM-47] - Adicionando TypeAheadFlutter e funcionalidades do controllador para o TypeAhead

---
+++ lib/app/modules/mural-divulgacao/pages/venda_materiais/pages/procura_materias/procura_materiais_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:system_maua_front/app/modules/mural-divulgacao/pages/venda_materiais/pages/procura_materias/procura_materiais_controller.dart';
+
+class ProcuraMateriaisPage extends StatefulWidget {
+  ProcuraMateriaisPage({Key? key}) : super(key: key);
+
+  @override
+  _ProcuraMateriaisPageState createState() => _ProcuraMateriaisPageState();
+}
+
+class _ProcuraMateriaisPageState
+    extends ModularState<ProcuraMateriaisPage, ProcuraMateriaisController> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
--- lib/app/modules/mural-divulgacao/pages/venda_materiais/pages/procura_materias/procura_materias_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:mobx/mobx.dart';
-
-part 'procura_materias_controller.g.dart';
-
-class ProcuraMateriasController = ProcuraMateriasControllerBase
-    with _$ProcuraMateriasController;
-
-abstract class ProcuraMateriasControllerBase with Store {}
