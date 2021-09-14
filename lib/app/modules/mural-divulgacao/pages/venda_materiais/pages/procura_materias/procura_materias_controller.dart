1571da5bf749256fcc1b621bda5ebb3e39d919bc
GabrielGodoy01 <gabriel.godoybz@hotmail.com>
2021-09-14 12:10:30 -0300

    [FM-47] - Estruturação MVC

---
+++ lib/app/modules/mural-divulgacao/pages/venda_materiais/pages/procura_materias/procura_materias_controller.dart
@@ -0,0 +1,8 @@
+import 'package:mobx/mobx.dart';
+
+part 'procura_materias_controller.g.dart';
+
+class ProcuraMateriasController = ProcuraMateriasControllerBase
+    with _$ProcuraMateriasController;
+
+abstract class ProcuraMateriasControllerBase with Store {}
