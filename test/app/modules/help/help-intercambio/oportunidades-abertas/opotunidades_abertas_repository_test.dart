654f1d0f2c98a11683815d1bff0294b82f7c95bb
Gustavo Consoleti <51282187+Gustavo-CRS@users.noreply.github.com>
2021-09-23 16:36:55 -0300

    [FM-26] testes unitários do controller e repositório finalizados

---
+++ test/app/modules/help/help-intercambio/oportunidades-abertas/opotunidades_abertas_repository_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-intercambio/oportunidades_abertas/repository/oportunidade_repository.dart';
+
+void main() {
+  late OportunidadeRepository oportunidadeRepository;
+
+  setUpAll(() {
+    oportunidadeRepository = OportunidadeRepository();
+  });
+
+  test('[TEST] - getUniversidade', () {
+    expect(oportunidadeRepository.listaDeUniversidade.isNotEmpty, true);
+  });
+}
