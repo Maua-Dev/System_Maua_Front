b6fecfc76859102c2d1669d359564f5a040354a7
Gustavo Consoleti <51282187+Gustavo-CRS@users.noreply.github.com>
2021-09-23 15:51:05 -0300

    criação do teste unitário do Intercambio Module

---
+++ test/app/modules/help/help-intercambio/help_intercambio_module_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_modular_test/flutter_modular_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-intercambio/help_intercambio_module.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-intercambio/oportunidades_abertas/controller/oportunidade_controller.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-intercambio/oportunidades_abertas/repository/oportunidade_repository.dart';
+
+void main() {
+  initModule(HelpIntercambioModule());
+
+  test('[TEST] - Injeção do OportunidadeRespotiory ', () {
+    Modular.get<OportunidadeRepository>();
+  });
+
+  test('[TEST] - Injeção do OportunidadeController', () {
+    var oportunidadeController = Modular.get<OportunidadeController>();
+    expect(oportunidadeController.repository,
+        isInstanceOf<OportunidadeRepository>());
+  });
+}
