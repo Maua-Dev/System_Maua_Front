95ddf4ef9a6609057cb3825b9b50a87a13783c44
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2021-09-24 00:29:03 -0300

    [FM-18] - Criação de widget para container de guia

---
+++ lib/app/modules/help/pages/help-estagio/widgets/help_estagio_card.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:system_maua_front/app/shared/themes/app_colors.dart';
+
+class HelpEstagioCard extends StatelessWidget {
+  final Function()? onTap;
+
+   HelpEstagioCard({
+     Key? key,
+    this.onTap,
+  }) : super (key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        decoration: BoxDecoration(
+          boxShadow: [AppColors.shadow],
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          ),
+          width: 355,
+          height: 50,
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Text(
+                    'Guia: como iniciar um estágio',
+                    style: TextStyle(
+                    color: Colors.black,
+                    ),
+                    ),
+                    ),
+                    ),
+                    Observer(builder: (_) {
+                      return GestureDetector(
+                        onTap: onTap,
+                        child: Icon(
+                          Icons.arrow_forward,
+                          color: Colors.black,
+                        ),
+                      );
+                    },),
+            ],
+          ),
+      ),
+    );
+    }
+}
