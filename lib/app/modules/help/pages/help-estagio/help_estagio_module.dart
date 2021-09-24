f196f600c5d98a7490387c3e77610c690e5c1006
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2021-09-24 16:12:37 -0300

    [FM-49] - Bindando o modulo do guia de estagio

--- lib/app/modules/help/pages/help-estagio/help_estagio_module.dart
+++ lib/app/modules/help/pages/help-estagio/help_estagio_module.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:system_maua_front/app/modules/help/pages/help-estagio/help_estagio_controller.dart';
 import 'package:system_maua_front/app/modules/help/pages/help-estagio/help_estagio_page.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-estagio/page/guia-de-estagio/guia_de_estagio_controller.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-estagio/page/guia-de-estagio/guia_de_estagio_page.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-estagio/page/guia-de-estagio/repositories/guia_de_estagio_repository.dart';
 import 'package:system_maua_front/app/modules/help/pages/help-estagio/repositories/help_estagio_repository.dart';
 
 class HelpEstagioModule extends Module {
@@ -8,11 +11,14 @@ class HelpEstagioModule extends Module {
   final List<Bind> binds = [
     Bind.lazySingleton((i) => HelpEstagioController(i())),
     Bind.lazySingleton<HelpEstagioRepository>((i) => HelpEstagioRepository()),
+    Bind.lazySingleton((i) => GuiaEstagioController(i())),
+    Bind.lazySingleton<GuiaEstagioRepository>((i) => GuiaEstagioRepository()),
   ];
 
   @override
   final List<ModularRoute> routes = [
     ChildRoute(Modular.initialRoute, child: (_,args) => HelpEstagioPage()),
+    ChildRoute('/guiaestagio', child: (_, args) => GuiaEstagioPage())
     
   ];
 }
