d2507d7639266ef83cd0b03aeb2add5547b3bad3
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2021-09-24 13:59:45 -0300

    [FM-18] - Controller arrumado

--- lib/app/modules/help/pages/help-estagio/help_estagio_controller.dart
+++ lib/app/modules/help/pages/help-estagio/help_estagio_controller.dart
@@ -1,4 +1,7 @@
 import 'package:mobx/mobx.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-estagio/models/help_estagio_model.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-estagio/models/help_estagio_panel_model.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-estagio/repositories/help_estagio_repository_interface.dart';
 
 part 'help_estagio_controller.g.dart';
 
@@ -6,5 +9,39 @@ class HelpEstagioController = _HelpEstagioControllerBase
     with _$HelpEstagioController;
 
 abstract class _HelpEstagioControllerBase with Store {
-  _HelpEstagioControllerBase();
+  IHelpEstagioRepository repository;
+
+  _HelpEstagioControllerBase(
+    this.repository,
+  ) {
+    getEstagio();
+  }
+
+  @observable
+  List<HelpEstagioModel> listaEstagio = List.empty();
+
+  @observable
+  List<HelpEstagioPanelModel> listaPanelEstagio = [];
+
+  @action
+  Future<void> getEstagio() async {
+    listaEstagio = await repository.getEstagio();
+    getListaPanelEstagio();
+  }
+
+  @action 
+  void getListaPanelEstagio() {
+    var list = <HelpEstagioPanelModel>[];
+    for (var i = 0; i < listaEstagio.length; i++) {
+      list.add(HelpEstagioPanelModel(estagio: listaEstagio[i]));
+    }
+    listaPanelEstagio = list;
+  }
+
+  @action
+  void trocaOpen(int index) {
+    var lista = List<HelpEstagioPanelModel>.from(listaPanelEstagio);
+    lista[index].changeStateIsOpen();
+    listaPanelEstagio = lista;
+  }
 }
--- lib/app/modules/help/pages/help-estagio/help_estagio_controller.g.dart
+++ lib/app/modules/help/pages/help-estagio/help_estagio_controller.g.dart
@@ -9,10 +9,76 @@ part of 'help_estagio_controller.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$HelpEstagioController on _HelpEstagioControllerBase, Store {
+  final _$listaEstagioAtom =
+      Atom(name: '_HelpEstagioControllerBase.listaEstagio');
+
+  @override
+  List<HelpEstagioModel> get listaEstagio {
+    _$listaEstagioAtom.reportRead();
+    return super.listaEstagio;
+  }
+
+  @override
+  set listaEstagio(List<HelpEstagioModel> value) {
+    _$listaEstagioAtom.reportWrite(value, super.listaEstagio, () {
+      super.listaEstagio = value;
+    });
+  }
+
+  final _$listaPanelEstagioAtom =
+      Atom(name: '_HelpEstagioControllerBase.listaPanelEstagio');
+
+  @override
+  List<HelpEstagioPanelModel> get listaPanelEstagio {
+    _$listaPanelEstagioAtom.reportRead();
+    return super.listaPanelEstagio;
+  }
+
+  @override
+  set listaPanelEstagio(List<HelpEstagioPanelModel> value) {
+    _$listaPanelEstagioAtom.reportWrite(value, super.listaPanelEstagio, () {
+      super.listaPanelEstagio = value;
+    });
+  }
+
+  final _$getEstagioAsyncAction =
+      AsyncAction('_HelpEstagioControllerBase.getEstagio');
+
+  @override
+  Future<void> getEstagio() {
+    return _$getEstagioAsyncAction.run(() => super.getEstagio());
+  }
+
+  final _$_HelpEstagioControllerBaseActionController =
+      ActionController(name: '_HelpEstagioControllerBase');
+
+  @override
+  void getListaPanelEstagio() {
+    final _$actionInfo = _$_HelpEstagioControllerBaseActionController
+        .startAction(name: '_HelpEstagioControllerBase.getListaPanelEstagio');
+    try {
+      return super.getListaPanelEstagio();
+    } finally {
+      _$_HelpEstagioControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void trocaOpen(int index) {
+    final _$actionInfo = _$_HelpEstagioControllerBaseActionController
+        .startAction(name: '_HelpEstagioControllerBase.trocaOpen');
+    try {
+      return super.trocaOpen(index);
+    } finally {
+      _$_HelpEstagioControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
     return '''
-
+listaEstagio: ${listaEstagio},
+listaPanelEstagio: ${listaPanelEstagio}
     ''';
   }
 }
