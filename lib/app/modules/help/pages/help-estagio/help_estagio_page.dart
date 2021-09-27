1ef44bdeffcf9372273a357c6fded04b065e19c0
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2021-09-27 18:22:56 -0300

    [FM-49] - Arrumando page

--- lib/app/modules/help/pages/help-estagio/help_estagio_page.dart
+++ lib/app/modules/help/pages/help-estagio/help_estagio_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:fluttericon/font_awesome5_icons.dart';
 import 'package:system_maua_front/app/modules/help/pages/help-estagio/help_estagio_controller.dart';
 import 'package:system_maua_front/app/modules/help/pages/help-estagio/widgets/help_estagio_card.dart';
 import 'package:system_maua_front/app/modules/help/widgets/help_panel_card_widget.dart';
 import 'package:system_maua_front/app/shared/components/app_bar/app_bar_widget.dart';
+import 'package:system_maua_front/app/shared/enumerates/arquivos_enum.dart';
 
 class HelpEstagioPage extends StatefulWidget {
   HelpEstagioPage({Key? key}) : super(key: key);
@@ -23,44 +23,47 @@ class _HelpEstagioPageState
         title: 'Estágio',
         icon: Icons.help_outline_outlined,
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: HelpEstagioCard(
-              onTap: () {
-                Modular.to.pushNamed('/help/estagio/guiaestagio');
-              },
-            )
-          ),
-          Center(
-            child: Text(
-              'Arquivos',
-            ),
-          ),
-          Expanded(
-            child: Observer(builder: (_) {
-              return ListView.builder(
-                shrinkWrap: true,
-                itemCount: controller.listaPanelEstagio.length,
-                itemBuilder: (context, index) => HelpPanelCard(
-                  titulo:
-                      controller.listaPanelEstagio[index].estagio.tituloPanel,
-                  descricao: Icon(FontAwesome5.file_pdf, color: Colors.red,),
-                  descrica:
-                      controller.listaPanelEstagio[index].estagio.nomearquivo!.nome,
-                  isOpen: controller.listaPanelEstagio[index].isOpen,
-                  onPressed: () {
-                    controller.trocaOpen(index);
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: HelpEstagioCard(
+                  onTap: () {
+                    Modular.to.pushNamed('/help/estagio/guiaestagio');
                   },
-                ),
-              );
-            }),
-          ),
-          SizedBox(
-            height: 32,
-          )
-        ],
+                )),
+            Center(
+              child: Text(
+                'Arquivos',
+              ),
+            ),
+            Expanded(
+              child: Observer(builder: (_) {
+                return ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: controller.listaPanelEstagio.length,
+                  itemBuilder: (context, index) => HelpPanelCard(
+                    titulo:
+                        controller.listaPanelEstagio[index].estagio.tituloPanel,
+                    descricao: controller.listaPanelEstagio[index].estagio
+                        .nomearquivo!.arquivo!.iconeArquivo,
+                    descrica: controller
+                        .listaPanelEstagio[index].estagio.nomearquivo!.nome,
+                    isOpen: controller.listaPanelEstagio[index].isOpen,
+                    onPressed: () {
+                      controller.trocaOpen(index);
+                    },
+                  ),
+                );
+              }),
+            ),
+            SizedBox(
+              height: 32,
+            )
+          ],
+        ),
       ),
     );
   }
