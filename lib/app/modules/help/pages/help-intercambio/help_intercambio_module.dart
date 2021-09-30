794e27649d6a64ebbaf5f1b511b82bb13fb44426
Gustavo Consoleti <51282187+Gustavo-CRS@users.noreply.github.com>
2021-09-30 15:58:31 -0300

    [FM-32] mudança no módulo e criação do caminho para FAQ page

--- lib/app/modules/help/pages/help-intercambio/help_intercambio_module.dart
+++ lib/app/modules/help/pages/help-intercambio/help_intercambio_module.dart
@@ -1,5 +1,8 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:system_maua_front/app/modules/help/pages/help-intercambio/help_intercambio_page.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-intercambio/pages/faq/controller/faq_intercambio_controller.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-intercambio/pages/faq/page/intercambio_faq_page.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-intercambio/pages/faq/repository/faq_repository.dart';
 import 'pages/oportunidades_abertas/controller/oportunidade_controller.dart';
 import 'pages/oportunidades_abertas/page/intercambio_oportunidades_page.dart';
 import 'pages/oportunidades_abertas/repository/oportunidade_repository.dart';
@@ -9,11 +12,14 @@ class HelpIntercambioModule extends Module {
   final List<Bind> binds = [
     Bind.lazySingleton((i) => OportunidadeRepository()),
     Bind.lazySingleton((i) => OportunidadeController(i())),
+    Bind.lazySingleton((i) => FaqIntercambioRepository()),
+    Bind.lazySingleton((i) => FaqIntercambioController(i())),
   ];
 
   @override
   final List<ModularRoute> routes = [
     ChildRoute(Modular.initialRoute, child: (_, args) => HelpIntercambioPage()),
     ChildRoute('/oportunidades', child: (_, args) => OportunidadePage()),
+    ChildRoute('/faq', child: (_, args) => FaqIntercambioPage()),
   ];
 }
