d7c451d496c9c99c02a1574887d136f2cea65b59
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2021-09-30 00:36:23 -0300

    [FM-49] - Unit test arrumado

--- test/app/modules/help/help-estagio/help_estagio_module_test.dart
+++ test/app/modules/help/help-estagio/help_estagio_module_test.dart
@@ -1,12 +1,22 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular_test/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-estagio/help_estagio_controller.dart';
 import 'package:system_maua_front/app/modules/help/pages/help-estagio/help_estagio_module.dart';
 import 'package:system_maua_front/app/modules/help/pages/help-estagio/page/guia-de-estagio/guia_de_estagio_controller.dart';
 import 'package:system_maua_front/app/modules/help/pages/help-estagio/page/guia-de-estagio/repositories/guia_de_estagio_repository.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-estagio/repositories/help_estagio_repository.dart';
 
 void main() {
   initModule(HelpEstagioModule());
+
+  test('[TEST] - Injeção do HelpEstagioController', () {
+    Modular.get<HelpEstagioController>();
+  });
+
+  test('[TEST] - Injeção do HelpEstagioRepository', () {
+    Modular.get<HelpEstagioRepository>();
+  });
 
   test('[TEST] - Injeção do GuiaEstagioController', () {
     var guiaEstagioController = Modular.get<GuiaEstagioController>();
