0852b7f60cb2403c5c23fe97af7de2e386e3f047
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2021-09-30 00:07:15 -0300

    [FM-18] - widget card retirado

--- lib/app/modules/help/pages/help-estagio/widgets/help_estagio_card.dart
+++ lib/app/modules/help/pages/help-estagio/widgets/help_estagio_card.dart
@@ -11,37 +11,35 @@ class HelpEstagioCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        decoration: BoxDecoration(
-          boxShadow: [AppColors.shadow],
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        width: MediaQuery.of(context).size.width * 0.95,
-        height: MediaQuery.of(context).size.height * 0.065,
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Center(
-                child: Text(
-                  'Guia: como iniciar um estágio',
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [AppColors.shadow],
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      width: MediaQuery.of(context).size.width * 0.95,
+      height: MediaQuery.of(context).size.height * 0.065,
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Text(
+                'Guia: como iniciar um estágio',
+                style: TextStyle(
+                  color: Colors.black,
                 ),
               ),
             ),
-            GestureDetector(
-              onTap: onTap,
-              child: Icon(
-                Icons.arrow_forward,
-                color: Colors.black,
-              ),
+          ),
+          GestureDetector(
+            onTap: onTap,
+            child: Icon(
+              Icons.arrow_forward,
+              color: Colors.black,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
