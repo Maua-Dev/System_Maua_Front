dac7e3a2c182585448780cab997581becd9b131f
Gustavo Consoleti <51282187+Gustavo-CRS@users.noreply.github.com>
2021-10-07 14:51:33 -0300

    [FM-32] ajuste nas binds e nos imports do módulo

--- lib/app/modules/help/pages/help-intercambio/help_intercambio_module.dart
+++ lib/app/modules/help/pages/help-intercambio/help_intercambio_module.dart
@@ -3,6 +3,8 @@ import 'package:system_maua_front/app/modules/help/pages/help-intercambio/help_i
 import 'package:system_maua_front/app/modules/help/pages/help-intercambio/pages/faq/controller/faq_intercambio_controller.dart';
 import 'package:system_maua_front/app/modules/help/pages/help-intercambio/pages/faq/page/intercambio_faq_page.dart';
 import 'package:system_maua_front/app/modules/help/pages/help-intercambio/pages/faq/repository/faq_repository.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-intercambio/pages/faq/repository/faq_repository_interface.dart';
+import 'package:system_maua_front/app/modules/help/pages/help-intercambio/pages/oportunidades_abertas/repository/oportunidade_repository_interface.dart';
 import 'pages/oportunidades_abertas/controller/oportunidade_controller.dart';
 import 'pages/oportunidades_abertas/page/intercambio_oportunidades_page.dart';
 import 'pages/oportunidades_abertas/repository/oportunidade_repository.dart';
@@ -10,9 +12,11 @@ import 'pages/oportunidades_abertas/repository/oportunidade_repository.dart';
 class HelpIntercambioModule extends Module {
   @override
   final List<Bind> binds = [
-    Bind.lazySingleton((i) => OportunidadeRepository()),
+    Bind.lazySingleton<IOportunidadeRepository>(
+        (i) => OportunidadeRepository()),
     Bind.lazySingleton((i) => OportunidadeController(i())),
-    Bind.lazySingleton((i) => FaqIntercambioRepository()),
+    Bind.lazySingleton<IFaqIntercambioRepository>(
+        (i) => FaqIntercambioRepository()),
     Bind.lazySingleton((i) => FaqIntercambioController(i())),
   ];
 
