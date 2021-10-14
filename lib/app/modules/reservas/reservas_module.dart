3af4660d02baa25a527b1cedecc30f080f38fcac
Felipe-16 <56928106+Felipe-16@users.noreply.github.com>
2021-10-14 19:30:46 -0300

    [FM-81] - Estruturação MVC

---
+++ lib/app/modules/reservas/reservas_module.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class ReservasModule extends Module{} 
---
+++ lib/app/modules/reservas/reservas_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ReservasPage extends StatefulWidget {
+  ReservasPage({Key? key}) : super(key: key);
+
+  @override
+  _ReservasPageState createState() => _ReservasPageState();
+}
+
+class _ReservasPageState extends State<ReservasPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+} 
