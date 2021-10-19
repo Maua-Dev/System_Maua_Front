cd17a4c7bd8cf30d83862da4c32841cac8664952
Felipe Ros Pegini <56928106+Felipe-16@users.noreply.github.com>
2021-10-19 18:43:50 -0300

    [fm-83] - formatação

--- test/app/modules/reservas/reservas-fablab/reservas_fablab_controller_test.dart
+++ test/app/modules/reservas/reservas-fablab/reservas_fablab_controller_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:system_maua_front/app/modules/reservas/models/reservas_fablab_model.dart';
-import 'package:system_maua_front/app/modules/reservas/pages/reservas-quadras/reservas_quadras_controller.dart';
 import 'package:system_maua_front/app/modules/reservas/pages/reservas_fablab/enumerates/fablab_enum.dart';
 import 'package:system_maua_front/app/modules/reservas/pages/reservas_fablab/repositories/reservas_fablab_repository_interface.dart';
 import 'package:system_maua_front/app/modules/reservas/pages/reservas_fablab/reservas_fablab_controller.dart';
