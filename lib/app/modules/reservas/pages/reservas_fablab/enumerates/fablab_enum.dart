2eb851eb854b8a75041f531d2b9ec19131392f6f
Felipe Ros Pegini <56928106+Felipe-16@users.noreply.github.com>
2021-10-19 18:33:51 -0300

    [FM-83]- Module + enum

---
+++ lib/app/modules/reservas/pages/reservas_fablab/enumerates/fablab_enum.dart
@@ -0,0 +1,16 @@
+enum FablabEnum { CORTELASER, CAMARAPINTURA, CORTELIXA, IMPRESSORA3D}
+
+extension FablabEnumExtension on FablabEnum {
+  String get titulo {
+    switch (this) {
+      case FablabEnum.CORTELASER:
+        return 'Corte a laser';
+      case FablabEnum.CAMARAPINTURA:
+        return 'Camara de pintura';
+      case FablabEnum.CORTELIXA:
+        return 'Equipamento de corte a lixa';
+      case FablabEnum.IMPRESSORA3D:
+        return 'Impressora 3D';
+    }
+  }
+}
