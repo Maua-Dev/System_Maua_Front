1df4cb9c24a58231995af14c676dce4e9a31bf85
Caio Toledo <caio.detoledo15@gmail.com>
2021-10-19 23:57:18 -0300

    [FM-8] - passando nome da materia por argumento

--- lib/app/modules/notas/notas_module.dart
+++ lib/app/modules/notas/notas_module.dart
@@ -2,6 +2,9 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:system_maua_front/app/modules/notas/pages/atingir-metas/atingir_metas_controller.dart';
 import 'package:system_maua_front/app/modules/notas/pages/atingir-metas/atingir_metas_page.dart';
 import 'package:system_maua_front/app/modules/notas/pages/atingir-metas/repositories/atingir_metas_repository.dart';
+import 'package:system_maua_front/app/modules/notas/pages/estatisticas/estatisticas.dart';
+import 'package:system_maua_front/app/modules/notas/pages/estatisticas/estatisticas_controller.dart';
+import 'package:system_maua_front/app/modules/notas/pages/estatisticas/repositories/estatisticas_repository.dart';
 import 'package:system_maua_front/app/modules/notas/pages/medias/medias_controller.dart';
 import 'package:system_maua_front/app/modules/notas/pages/medias/medias_page.dart';
 import 'package:system_maua_front/app/modules/notas/pages/medias/repositories/filtros/filtros_medias_repository.dart';
@@ -25,12 +28,18 @@ class NotasModule extends Module {
     Bind.lazySingleton<NotasAlunoRepository>((i) => NotasAlunoRepository()),
     Bind.lazySingleton((i) => AtingirMetasController(i())),
     Bind.lazySingleton<AtingirMetasRepository>((i) => AtingirMetasRepository()),
+    Bind.lazySingleton((i) => EstatisticasController(i())),
+    Bind.lazySingleton<EstatisticasRepository>((i) => EstatisticasRepository()),
   ];
 
   @override
   final List<ModularRoute> routes = [
     ChildRoute(Modular.initialRoute, child: (_, args) => MediasPage()),
-    ChildRoute('/notas', child: (_, args) => NotasAlunoPage()),
-    ChildRoute('/atingir-metas', child: (_, args) => AtingirMetasPage()),
+    ChildRoute('/notas',
+        child: (_, args) => NotasAlunoPage(nomeMateria: args.data)),
+    ChildRoute('/atingir-metas',
+        child: (_, args) => AtingirMetasPage(nomeMateria: args.data)),
+    ChildRoute('/estatisticas',
+        child: (_, args) => EstatisticasPage(nomeMateria: args.data)),
   ];
 }
