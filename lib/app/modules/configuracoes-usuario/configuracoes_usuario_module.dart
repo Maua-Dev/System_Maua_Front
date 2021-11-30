dd4a569a3595d748bcbe78bf025ed5b56284a445
Gabriel de Godoy Braz <gabriel.godoybz@hotmail.com>
2021-11-30 14:07:48 -0300

    Estruturação do modulo de configuracao-usuario

---
+++ lib/app/modules/configuracoes-usuario/configuracoes_usuario_module.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'configuracoes_usuario_page.dart';
+
+class ConfiguracoesUsuarioModule extends Module {
+  @override
+  final List<Bind> binds = [];
+
+  @override
+  final List<ModularRoute> routes = [
+    ChildRoute(
+      Modular.initialRoute,
+      child: (_, args) => ConfiguracoesUsuarioPage(),
+    ),
+  ];
+}
---
+++ lib/app/modules/configuracoes-usuario/configuracoes_usuario_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ConfiguracoesUsuarioPage extends StatefulWidget {
+  const ConfiguracoesUsuarioPage({Key? key}) : super(key: key);
+
+  @override
+  _ConfiguracoesUsuarioPageState createState() =>
+      _ConfiguracoesUsuarioPageState();
+}
+
+class _ConfiguracoesUsuarioPageState extends State<ConfiguracoesUsuarioPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
