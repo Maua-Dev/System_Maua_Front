181fe95eed6cc789f33975d27abf0ed5a2ddb0b0
Caio <ctoledo@pieracciani.com.br>
2022-05-22 23:23:09 -0300

    ajuste linter

--- lib/app/modules/materias/models/materias_model.dart
+++ lib/app/modules/materias/models/materias_model.dart
@@ -2,7 +2,7 @@ import 'package:system_maua_front/app/shared/enumerates/tipo_materia_enum.dart';
 
 class MateriasModel {
   final TipoMateriaEnum nome;
-  final double progresso; 
+  final double progresso;
 
   MateriasModel({required this.nome, required this.progresso});
 }
