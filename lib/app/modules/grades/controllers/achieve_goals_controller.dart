00be3341df3e76d3e962badacfee51fbabd50f5f
Caio <ctoledo@pieracciani.com.br>
2022-07-18 19:55:01 -0300

    linter

--- lib/app/modules/grades/controllers/achieve_goals_controller.dart
+++ lib/app/modules/grades/controllers/achieve_goals_controller.dart
@@ -29,46 +29,49 @@ abstract class _AchieveGoalsControllerBase with Store {
     var kt = averageToCalculate.taskHint;
     // soma de pesos de provas total
     var sumKpa = 0.0;
-    averageToCalculate.tests.forEach((test) { 
+    averageToCalculate.tests.forEach((test) {
       sumKpa += test.heft!;
     });
     // soma de pesos de trabalhos total
     var sumKtb = 0.0;
-    averageToCalculate.tasks.forEach((task) { 
+    averageToCalculate.tasks.forEach((task) {
       sumKtb += task.heft!;
     });
     // soma de pesos de provas não feitas
     var sumKpn = 0.0;
-    averageToCalculate.tests.forEach((test) { 
-      if(test.value == null){
+    averageToCalculate.tests.forEach((test) {
+      if (test.value == null) {
         sumKpn += test.heft!;
       }
     });
     // soma de pesos de trabalhos não feitos
     var sumKtm = 0.0;
-    averageToCalculate.tasks.forEach((task) { 
-      if(task.value == null){
+    averageToCalculate.tasks.forEach((task) {
+      if (task.value == null) {
         sumKtm += task.heft!;
       }
     });
 
     var averageTestsDone = 0.0;
     averageToCalculate.tests.forEach((test) {
-      if(test.value != null){
-        averageTestsDone += (kp * test.heft! * test.value!) / ((kp+kt) * sumKpa);
+      if (test.value != null) {
+        averageTestsDone +=
+            (kp * test.heft! * test.value!) / ((kp + kt) * sumKpa);
       }
     });
 
     var averageTasksDone = 0.0;
     averageToCalculate.tasks.forEach((task) {
-      if(task.value != null){
-        averageTasksDone += (kt * task.heft! * task.value!) / ((kp+kt) * sumKtb);
+      if (task.value != null) {
+        averageTasksDone +=
+            (kt * task.heft! * task.value!) / ((kp + kt) * sumKtb);
       }
     });
 
     var numerator = goal - averageTestsDone - averageTasksDone;
-    var denominator = (1/(kp+kt))*( (kp*sumKpn)/sumKpa + (kt*sumKtm)/sumKtb);
-    var achieveGoals = numerator/denominator;
+    var denominator =
+        (1 / (kp + kt)) * ((kp * sumKpn) / sumKpa + (kt * sumKtm) / sumKtb);
+    var achieveGoals = numerator / denominator;
     return achieveGoals;
   }
 }
